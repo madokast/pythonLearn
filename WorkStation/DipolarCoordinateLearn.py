e0fe8b100bc8dc989c83e6ab0be0de9e2e48c4ab
madokast <578562554@qq.com>
2019-07-02 11:17:46 +0800

    喵

---
+++ WorkStation/DipolarCoordinateLearn.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = 1  # foci 焦点 (0,a) (0,-a)
+
+# ksi 0-2pi 构成过一个焦点的圆，关于x轴对称，圆心在轴上，也就是说上下各一个
+# |eta| 的值越小，圆半径越大。 eta 不能为0
+# eta 为正时 圆整体位于x轴之上 。 复数时，位于x轴之下
+# 圆方程 x^2 + ( x - a cot(ksi))^2 = a^2/sin^2(ksi)
+ksi = np.linspace(0, 2 * np.pi, 100)
+for eta in np.linspace(-5, 5, 11):
+    if np.abs(eta) < 10e-7: continue
+    x = a * np.sin(ksi) / (np.cosh(eta) - np.cos(ksi))
+    y = a * np.sinh(eta) / (np.cosh(eta) - np.cos(ksi))
+    plt.plot(x, y)
+
+
+# eta 0-2pi 构成圆心在x轴上的圆 . 圆经过两个焦点
+# ksi 不能为0
+
+eta = np.linspace(0, 5, 100)
+for ksi in np.linspace(-5, 5, 11):
+    if np.abs(ksi) < 10e-7: continue
+    x = a * np.sin(ksi) / (np.cosh(eta) - np.cos(ksi))
+    y = a * np.sinh(eta) / (np.cosh(eta) - np.cos(ksi))
+    plt.plot(x, y)
+    plt.plot(x, -y)
+
+plt.axis('equal')
+plt.show()
